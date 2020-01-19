87f74a453bf5f8c5b45ab52bd923152af522dbd4
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2020-01-19 12:28:49 +0100

    Add mod base.

---
+++ src/main/java/fr/alasdiablo/adlib/ADLib.java
@@ -0,0 +1,22 @@
+package fr.alasdiablo.adlib;
+
+import fr.alasdiablo.adlib.utils.Reference;
+import net.minecraft.block.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(Reference.MOD_ID)
+public class ADLib {
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public ADLib() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        LOGGER.info("Starting ADlib.");
+    }
+}
